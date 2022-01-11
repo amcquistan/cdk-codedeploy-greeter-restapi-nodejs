b0a4169551119e6f15ee991269ef895f39c2c366
Adam McQuistan <sci.guy.mcq@gmail.com>
2022-01-11 15:55:06 -0600

    wip

--- src/greeter-api/install.sh
+++ src/greeter-api/install.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt install nodejs git nginx -y
+apt update -y
 
-git clone 
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+
+apt install nodejs git nginx -y
 
 npm i -g pm2
+
+git clone https://github.com/amcquistan/cdk-codedeploy-greeter-restapi-nodejs.git
+
+cd cdk-codedeploy-greeter-restapi-nodejs/src/greeter-api
+
+npm install
+
+pm2 start greeter-api-server.js
 
