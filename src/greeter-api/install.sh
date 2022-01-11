d264ef0dc8193cc6fee691004dda8241eb549f27
Adam McQuistan <sci.guy.mcq@gmail.com>
2022-01-11 16:16:31 -0600

    install shell script added

--- src/greeter-api/install.sh
+++ src/greeter-api/install.sh
@@ -4,15 +4,22 @@ apt update -y
 
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 
-apt install nodejs git nginx -y
+apt install nodejs nginx -y
 
 npm i -g pm2
 
-git clone https://github.com/amcquistan/cdk-codedeploy-greeter-restapi-nodejs.git
+# apt install git -y
+# git clone https://github.com/amcquistan/cdk-codedeploy-greeter-restapi-nodejs.git
 
-cd cdk-codedeploy-greeter-restapi-nodejs/src/greeter-api
+cd /home/ubuntu/cdk-codedeploy-greeter-restapi-nodejs/src/greeter-api
 
 npm install
 
 pm2 start greeter-api-server.js
 
+cp greeter-api-proxy.conf /etc/nginx/sites-available/
+
+ln -s /etc/nginx/sites-available/greeter-api-proxy.conf /etc/nginx/sites-enabled/greeter-api-proxy.conf
+rm /etc/nginx/sites-enabled/default
+
+systemctl restart nginx
